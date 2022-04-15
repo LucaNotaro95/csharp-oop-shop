39d5b74b72b8e6f370fc5141834919379a9a17d8
LucaNotaro95 <100934253+LucaNotaro95@users.noreply.github.com>
2022-04-15 08:40:56 +0200

    Add files via upload

---
+++ CSharp Shop/Program.cs
@@ -0,0 +1,13 @@
+﻿using CSharp_Shop;
+
+Prodotto carta = new Prodotto("Foglio","Carta per scrivere",3,0.23f);
+Console.WriteLine( carta.ToString());
+
+Prodotto macchinetta = new Prodotto("Macchinetta HOT WHEELS", "Giocattolo di una macchina con design fantastico", 10, 0.23f);
+Prodotto latte = new Prodotto("Latte", "Bevanda al alto valore nutritivo, specialmente calcio", 2, 0.04f);
+
+Console.WriteLine (latte.ToString());
+
+Console.WriteLine (macchinetta.ToString());
+
+Console.WriteLine(macchinetta.nomeEsteso());
